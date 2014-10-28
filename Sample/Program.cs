44d7fff816dbf7d06f68d8ef158c15e6d807681e
Eberhard Beilharz <eb1@sil.org>
2014-10-28 18:34:06 +0100

    Add sample project

---
+++ Sample/Program.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) 2014 SIL International
+// This software is licensed under the MIT License (http://opensource.org/licenses/MIT)
+using System;
+
+namespace Sample
+{
+	class A: IDisposable
+	{
+		public A()
+		{
+		}
+
+		public void Dispose()
+		{
+		}
+	}
+
+	class B: IDisposable
+	{
+		private A a;
+		public B()
+		{
+			a = new A();
+		}
+
+		public void Dispose()
+		{
+			// forget to dispose A
+		}
+	}
+
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			using (new B())
+			{}
+		}
+	}
+}
