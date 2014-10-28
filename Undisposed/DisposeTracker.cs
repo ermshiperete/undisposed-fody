a1717f4f1c248609ea7c416fcc3bdb965c915f13
Eberhard Beilharz <eb1@sil.org>
2014-10-28 15:04:20 +0100

    Improve object store
    Depending on the implementation of GetHashCode multiple objects
    may return the same hash code. This change makes use of
    RuntimeHelper.GetHashCode that returns a hash code based on the
    object location, so it should work better.

--- Undisposed/DisposeTracker.cs
+++ Undisposed/DisposeTracker.cs
@@ -2,13 +2,14 @@
 // This software is licensed under the MIT license (http://opensource.org/licenses/MIT)
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace Undisposed
 {
 	public static class DisposeTracker
 	{
 		private static Dictionary<Type, int> _Registrations = new Dictionary<Type, int>();
-		private static Dictionary<object, int> _ObjectNumber = new Dictionary<object, int>();
+		private static Dictionary<int, int> _ObjectNumber = new Dictionary<int, int>();
 		private static Dictionary<Type, List<int>> _UndisposedObjects = new Dictionary<Type, List<int>>();
 
 		public static TrackerOutputKind OutputKind { get; set; }
@@ -16,20 +17,21 @@ namespace Undisposed
 		public static void Reset()
 		{
 			_Registrations = new Dictionary<Type, int>();
-			_ObjectNumber = new Dictionary<object, int>();
+			_ObjectNumber = new Dictionary<int, int>();
 			_UndisposedObjects = new Dictionary<Type, List<int>>();
 		}
 
 		public static void Register(object obj)
 		{
 			var t = obj.GetType();
+			var hash = RuntimeHelpers.GetHashCode(obj);
 			if (!_Registrations.ContainsKey(t))
 			{
 				_Registrations.Add(t, 1);
 				_UndisposedObjects.Add(t, new List<int>());
 			}
 			var thisNumber = _Registrations[t]++;
-			_ObjectNumber.Add(obj, thisNumber);
+			_ObjectNumber.Add(hash, thisNumber);
 			_UndisposedObjects[t].Add(thisNumber);
 
 			if ((OutputKind & TrackerOutputKind.Registration) != 0)
@@ -39,8 +41,9 @@ namespace Undisposed
 		public static void Unregister(object obj)
 		{
 			var t = obj.GetType();
+			var hash = RuntimeHelpers.GetHashCode(obj);
 			int thisNumber;
-			if (!_ObjectNumber.TryGetValue(obj, out thisNumber))
+			if (!_ObjectNumber.TryGetValue(hash, out thisNumber))
 			{
 				Console.WriteLine("Disposing {0}: Error: Object was not registered", t.FullName);
 				return;
@@ -49,7 +52,7 @@ namespace Undisposed
 			if ((OutputKind & TrackerOutputKind.Registration) != 0)
 				Console.WriteLine("*** Disposing {0} {1}", t.FullName, thisNumber);
 
-			_ObjectNumber.Remove(obj);
+			_ObjectNumber.Remove(hash);
 			_UndisposedObjects[t].Remove(thisNumber);
 			if (_UndisposedObjects[t].Count == 0)
 				_UndisposedObjects.Remove(t);
